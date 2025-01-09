900e24c8f05904f008b2dbf8f9357c378014aae9
iheb-bennouri <iheb.bennouri@campion-tech.com>
2025-01-09 23:38:33 +0100

    Update

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -4,11 +4,10 @@ from app.config import SERVER_NAME
 login_url = f'http://{SERVER_NAME}/api/auth/login'
 
 
-def test_auth_success(client):
-    """Test successful login."""
-    response = client.post(login_url, json={'username': 'public', 'password': 'public'})
-    assert response.status_code == 200
-    assert 'token' in response.json
+def test_auth_required_fields(client):
+    """Test login with missing data."""
+    response = client.post(login_url, json={'username': 'admin'})
+    assert response.status_code == 400
 
 
 def test_auth_invalid_credentials(client):
@@ -17,7 +16,8 @@ def test_auth_invalid_credentials(client):
     assert response.status_code == 401
 
 
-def test_auth_required_fields(client):
-    """Test login with missing data."""
-    response = client.post(login_url, json={'username': 'admin'})
-    assert response.status_code == 400
+def test_auth_success(client):
+    """Test successful login."""
+    response = client.post(login_url, json={'username': 'public', 'password': 'public'})
+    assert response.status_code == 200
+    assert 'token' in response.json
